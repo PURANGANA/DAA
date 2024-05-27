9f9d184305f55f0f11196e3eb595ce9c1c42eb2f
PURANGANA <shuttlerpm04@gmail.com>
2024-05-27 23:13:49 +0530

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -1,42 +1,99 @@
+
 #include <stdio.h>
- 
-int n, i, j, visited[10], queue[10], front = -1, rear = -1;
-int adj[10][10];
- 
-void bfs(int v)
+
+#define MAX_NODES 100
+
+void addEdge(int u, int v, int adjMatrix[MAX_NODES][MAX_NODES]) //node value starts from 1
 {
-    for (i = 1; i <= n; i++)
-        if (adj[v][i] && !visited[i])
-            queue[++rear] = i;
-    if (front <= rear)
+    adjMatrix[u][v] = 1;
+    adjMatrix[v][u] = -1;
+}
+
+void displayMatrix(int nodes, int adjMatrix[MAX_NODES][MAX_NODES])
+{
+	int i, j;
+    printf("Adjacency Matrix:\n");
+    for (i = 0; i < nodes; i++)
     {
-        visited[queue[front]] = 1;
-        bfs(queue[front++]);
+        for (j = 0; j < nodes; j++)
+            printf("%3d ", adjMatrix[i][j]);
+        printf("\n");
     }
 }
- 
+
+void BFS (int startNode, int nodes, int adjMatrix[MAX_NODES][MAX_NODES])
+{
+	static int visited[MAX_NODES];
+	int queue[MAX_NODES], current, front = 0, rear = 0, i;
+	
+	visited[startNode] = 1;
+	queue[rear++] = startNode;
+	
+	printf("BFS Traversal starting from node %d: ",startNode+1);
+	
+	while(front != rear)
+	{
+		current = queue[front++];
+		printf("%d ",current + 1);
+		
+		for(i = 0; i < nodes; i++)
+		{
+			if(adjMatrix[current][i] == 1 && visited[i] == 0)
+			{
+				visited[i] = 1;
+				queue[rear++] = i;
+			}
+		}
+	}	
+}
+
 int main()
 {
-    int v;
-    printf("Enter the number of vertices: ");
-    scanf("%d", &n);
-    for (i = 1; i <= n; i++)
+    int i, nodes, edges, u, v;
+    static int adjMatrix[MAX_NODES][MAX_NODES];
+
+    printf("Enter number of nodes in the graph: ");
+    scanf("%d", &nodes);
+
+    printf("Enter number of edges in the graph: ");
+    scanf("%d", &edges);
+
+    printf("Enter the edges (format: from_node to_node) (node value starts from 1):\n"); //node value starts from 1
+    for (i = 0; i < edges; i++)
     {
-        queue[i] = 0;
-        visited[i] = 0;
+        scanf("%d %d", &u, &v);
+        addEdge(u-1, v-1, adjMatrix);
     }
-    printf("Enter graph data in matrix form:    \n");
-    for (i = 1; i <= n; i++)
-        for (j = 1; j <= n; j++)
-            scanf("%d", &adj[i][j]);
-    printf("Enter the starting vertex: ");
-    scanf("%d", &v);
-    bfs(v);
-    printf("The node which are reachable are:    \n");
-    for (i = 1; i <= n; i++)
-        if (visited[i])
-            printf("%d\t", i);
-        else
-            printf("BFS is not possible. Not all nodes are reachable");
+
+    displayMatrix(nodes, adjMatrix);
+    
+    printf("Enter the starting node for BFS traversal: ");
+    scanf("%d", &u);
+    BFS(u-1, nodes, adjMatrix);
+    
     return 0;
 }
+
+/*
+Output:
+------
+Enter number of nodes in the graph: 5
+Enter number of edges in the graph: 5
+Enter the edges (format: from_node to_node) (node value starts from 1):
+1 2
+1 3
+1 4
+3 4
+3 5
+Adjacency Matrix:
+  0   1   1   1   0
+ -1   0   0   0   0
+ -1   0   0   1   1
+ -1   0  -1   0   0
+  0   0  -1   0   0
+Enter the starting node for BFS traversal: 1
+BFS Traversal starting from node 1: 1 2 3 4 5
+--------------------------------
+Process exited after 28.34 seconds with return value 0
+Press any key to continue . . .
+*/
